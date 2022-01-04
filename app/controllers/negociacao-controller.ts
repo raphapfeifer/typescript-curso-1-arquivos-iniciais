323f43297f18ef1cc9a8ea994416a654aa3283da
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-01-04 15:07:00 -0300

    Tipagem de métodos e atributos

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,9 @@
 import { Negociacao } from "../models/negociacao.js";
 
 export class NegociacaoController{
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
 
     constructor(){
         this.inputData = document.querySelector('#data');
@@ -12,13 +12,16 @@ export class NegociacaoController{
     }
 
 
-    adiciona(){
-        const negociacao = new Negociacao(
-            this.inputData.value,
-            this.inputQuantidade.value,
-            this.inputValor.value
-        );
-
+    adiciona(): void{
+        const negociacao = this.criaNegociacao();
         console.log(negociacao);
+    }
+
+    criaNegociacao(): Negociacao{
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp,','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date,quantidade,valor);
     }
 }
