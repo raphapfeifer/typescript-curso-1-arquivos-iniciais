e29a66e574169b31f4df892670b66b62356600e3
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-01-07 16:11:55 -0300

    Criação do método adiciona na classe negociações

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,5 +2,13 @@ import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes{
     private negociacoes: Array<Negociacao> = [];
-}
 
+    adiciona(negociacao: Negociacao): void{
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao>{
+        return this.negociacoes;
+    }
+
+}
