af00ba44a1650501b563d009c82ed462af171b4d
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-01-14 10:32:31 -0300

    Criação da view

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,9 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-views.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
-})
+});
